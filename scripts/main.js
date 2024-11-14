b580b3d4db283e4215d70155abe6d547283525d0
hamzah <hamzah1faisal@gmail.com>
2024-11-14 01:27:54 +0000

    implemented function to print out book details from myLibrary array and present them in a table

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+let t = "";
 
 function Book(title, author, pages, read)
 {
@@ -15,3 +16,15 @@ function addBookToLibrary(title, author, pages, read)
 }
 
 addBookToLibrary("Book", "author", "pages", "read");
+addBookToLibrary("Book", "author", "pages", "read");
+
+myLibrary.forEach((book)=>{
+    var tr = "<tr>";
+    tr += "<td>"+book.title+"</td>";
+    tr += "<td>"+book.author+"</td>";
+    tr += "<td>"+book.pages+"</td>";
+    tr += "<td>"+book.read+"</td>";
+    tr += "</tr>";
+    t += tr;
+})
+document.getElementById("table").innerHTML += t;
