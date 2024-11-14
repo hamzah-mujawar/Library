d0abe3cf16b14a42b4a3e8aa03363a8d9a082c67
hamzah <hamzah1faisal@gmail.com>
2024-11-14 00:46:47 +0000

    created functions to add a book object to the library which is an array

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read)
+{
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read)
+{
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+addBookToLibrary("Book", "author", "pages", "read");
